18c0dc8cd5346b7bfbc64864ae212e6b2db26fbd
Viatrak <justin.rohrer@corviatech.com>
2016-08-20 14:57:00 -0500

    Version update

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-tenant-users',
-    version='0.2.2',
+    version='0.2.3',
     packages=[
         'tenant_users',
         'tenant_users.companies',
