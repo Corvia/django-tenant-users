e7f3dd3ee1da47db982086d7370a0b78e0ae3031
Viatrak <justin.rohrer@corviatech.com>
2016-08-25 11:02:38 -0500

    Update migrations

--- tenant_users/permissions/migrations/0001_initial.py
+++ tenant_users/permissions/migrations/0001_initial.py
@@ -2,8 +2,8 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
-from django.conf import settings
 import tenant_users.permissions.models
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
@@ -20,9 +20,9 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(primary_key=True, serialize=False, auto_created=True, verbose_name='ID')),
                 ('is_superuser', models.BooleanField(default=False, verbose_name='superuser status', help_text='Designates that this user has all permissions without explicitly assigning them.')),
                 ('is_staff', models.BooleanField(default=False, verbose_name='staff status', help_text='Designates whether the user can log into this tenants admin site.')),
-                ('groups', models.ManyToManyField(blank=True, related_name='user_set', related_query_name='user', help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', verbose_name='groups', to='auth.Group')),
+                ('groups', models.ManyToManyField(related_name='user_set', related_query_name='user', blank=True, to='auth.Group', verbose_name='groups', help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.')),
                 ('profile', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
-                ('user_permissions', models.ManyToManyField(blank=True, related_name='user_set', related_query_name='user', help_text='Specific permissions for this user.', verbose_name='user permissions', to='auth.Permission')),
+                ('user_permissions', models.ManyToManyField(related_name='user_set', related_query_name='user', blank=True, to='auth.Permission', verbose_name='user permissions', help_text='Specific permissions for this user.')),
             ],
             options={
                 'abstract': False,
