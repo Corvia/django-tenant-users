38768397a75ae1424fe5d587e605fa6f4c9923dd
viatrak <justin.rohrer@corviatech.com>
2017-02-24 20:39:13 -0600

    Version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-tenant-users',
-    version='0.2.21',
+    version='0.2.22',
     packages=[
         'tenant_users',
         'tenant_users.tenants',
