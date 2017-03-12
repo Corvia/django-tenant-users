787eefe992c3344f37422993595ff93731803443
viatrak <justin.rohrer@corviatech.com>
2017-03-12 15:42:18 -0500

    Bump version for PR#5. Update PyPi package build

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-tenant-users',
-    version='0.3.4',
+    version='0.3.5',
     packages=[
         'tenant_users',
         'tenant_users.tenants',
