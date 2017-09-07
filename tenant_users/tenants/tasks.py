d6fb1a29645072e42fe948d0c075035b287959c6
viatrak <justin@milker.io>
2017-09-07 09:57:58 -0500

    Add schema context wrapper around provision task to guarantee schema consistency

--- tenant_users/tenants/tasks.py
+++ tenant_users/tenants/tasks.py
@@ -2,7 +2,8 @@ import time
 from django.conf import settings
 from django.contrib.auth import get_user_model
 
-from ..compat import get_tenant_model, TENANT_SCHEMAS, get_tenant_domain_model
+from ..compat import (get_tenant_model, TENANT_SCHEMAS, get_public_schema_name,
+                      get_tenant_domain_model, schema_context)
 
 from .models import InactiveError, ExistsError
 
@@ -42,29 +43,31 @@ def provision_tenant(tenant_name, tenant_slug, user_email, is_staff=False):
 
     # noinspection PyBroadException
     try:
+        # Wrap it in public schema context so schema consistency is maintained
+        # if any error occurs
+        with schema_context(get_public_schema_name()):
+            if TENANT_SCHEMAS:
+                # Create a TenantModel object and tenant schema
+                tenant = TenantModel.objects.create(
+                    name=tenant_name,
+                    slug=tenant_slug,
+                    domain_url=tenant_domain,
+                    schema_name=schema_name,
+                    owner=user,
+                )
 
-        if TENANT_SCHEMAS:
-            # Create a TenantModel object and tenant schema
-            tenant = TenantModel.objects.create(
-                name=tenant_name,
-                slug=tenant_slug,
-                domain_url=tenant_domain,
-                schema_name=schema_name,
-                owner=user,
-            )
+            else:  # django-tenants
+                tenant = TenantModel.objects.create(name=tenant_name,
+                                                    slug=tenant_slug,
+                                                    schema_name=schema_name,
+                                                    owner=user)
 
-        else:  # django-tenants
-            tenant = TenantModel.objects.create(name=tenant_name,
-                                                slug=tenant_slug,
-                                                schema_name=schema_name,
-                                                owner=user)
-
-            # Add one or more domains for the tenant
-            domain = get_tenant_domain_model().objects.create(domain=tenant_domain,
-                                                              tenant=tenant,
-                                                              is_primary=True)
-        # Add user as a superuser inside the tenant
-        tenant.add_user(user, is_superuser=True, is_staff=is_staff)
+                # Add one or more domains for the tenant
+                domain = get_tenant_domain_model().objects.create(domain=tenant_domain,
+                                                                  tenant=tenant,
+                                                                  is_primary=True)
+            # Add user as a superuser inside the tenant
+            tenant.add_user(user, is_superuser=True, is_staff=is_staff)
     except:
         if domain is not None:
             domain.delete()
