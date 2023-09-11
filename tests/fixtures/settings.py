7b0996b8f64a743a43f48ef32b75a3aa0e376369
Wizely99 <100375920+Wizely99@users.noreply.github.com>
2023-09-11 12:40:13 +0300

    pytest fixture for multi tenant types

--- tests/fixtures/settings.py
+++ tests/fixtures/settings.py
@@ -21,3 +21,44 @@ def _debug(settings) -> None:
     settings.DEBUG = False
     for template in settings.TEMPLATES:
         template['OPTIONS']['debug'] = True
+
+
+@pytest.fixture(autouse=False)
+def tenant_type_settings(settings):
+    settings.HAS_MULTI_TYPE_TENANTS = True
+    settings.MULTI_TYPE_DATABASE_FIELD = "tenant_type"
+
+    settings.TENANT_MODEL = 'companies.CompanyWithType'
+    settings.TENANT_DOMAIN_MODEL = 'companies.DomainWithType'
+
+    settings.TENANT_TYPES = {
+        "public": {
+            "APPS": [
+                'django_tenants',
+                'django.contrib.admin',
+                'django.contrib.auth',
+                'django.contrib.contenttypes',
+                'django.contrib.sessions',
+                'django.contrib.messages',
+                'django.contrib.staticfiles',
+                'tenant_users.permissions',
+                'tenant_users.tenants',
+                'django_test_app.companies',
+                'django_test_app.users',
+            ]
+        },
+        "example_type": {
+            "APPS": [
+                'django.contrib.auth',
+                'django.contrib.contenttypes',
+                'tenant_users.permissions',
+            ]
+        },
+    }
+    apps = []
+    for schema in settings.TENANT_TYPES:
+        apps += [
+            app for app in settings.TENANT_TYPES[schema]["APPS"] if app not in apps
+        ]
+    settings.INSTALLED_APPS = apps
+    # YourModel.add_to_class('dynamic_field', models.CharField(max_length=100))
