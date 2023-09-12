b659fa38f0396a884d9720c345122d5493acabd2
Mike <mike@zivix.com>
2023-09-12 16:37:11 -0700

    refactor: remove redundant django_db markers

--- tests/test_tenants/test_utils.py
+++ tests/test_tenants/test_utils.py
@@ -5,7 +5,6 @@ from tenant_users.tenants import utils
 from tenant_users.tenants.models import ExistsError
 
 
-@pytest.mark.django_db()
 def test_get_current_tenant(public_tenant, test_tenants):
     """Tests utils.get_current_tenant() for correctness."""
     with schema_context(get_public_schema_name()):
@@ -19,7 +18,6 @@ def test_get_current_tenant(public_tenant, test_tenants):
         assert current_tenant == tenant
 
 
-@pytest.mark.django_db()
 def test_duplicate_tenant_url(tenant_user):
     """Tests duplicate public tenant error."""
     with pytest.raises(ExistsError, match='Public tenant already exists'):
