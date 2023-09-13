8e4d67e370f0780b334485c1ace6b2e16f650b66
Mike <mike@zivix.com>
2023-09-12 17:45:07 -0700

    refactor: Convert class-based tests to function-based

--- tests/test_tenants/test_models/test_userprofile.py
+++ tests/test_tenants/test_models/test_userprofile.py
@@ -9,34 +9,34 @@ from tenant_users.tenants.models import DeleteError
 TenantUser = get_user_model()
 
 
-class TestUserProfile(django.TestCase):
-    """This is a property-based test that ensures model correctness."""
+@pytest.mark.django_db()
+@given(django.from_model(TenantUser))
+@settings(deadline=None)
+def test_model_properties(instance: TenantUser) -> None:
+    """Tests that instance can be saved and has correct representation."""
+    instance.save()
 
-    @given(django.from_model(TenantUser))
-    @settings(deadline=None)
-    def test_model_properties(self, instance: TenantUser) -> None:
-        """Tests that instance can be saved and has correct representation."""
-        instance.save()
+    # Test UserProfile.has_verified_email()
+    assert instance.has_verified_email() == instance.is_verified
 
-        # Test UserProfile.has_verified_email()
-        assert instance.has_verified_email() == instance.is_verified
+    # Test UserProfile.get_full_name()
+    assert instance.get_full_name() == str(instance)
 
-        # Test UserProfile.get_full_name()
-        assert instance.get_full_name() == str(instance)
+    # Test UserProfile.get_short_name()
+    assert instance.get_short_name() == instance.email
 
-        # Test UserProfile.get_short_name()
-        assert instance.get_short_name() == instance.email
 
-    @given(django.from_model(TenantUser))
-    def test_user_delete(self, instance: TenantUser) -> None:
-        """Tests tenants.models.UserProfile.delete scenarios."""
-        # Test UserProfile.delete()
-        with pytest.raises(
-            DeleteError,
-            match=r'delete_user\(\) should be used',
-        ):
-            instance.delete()
+@pytest.mark.django_db()
+@given(django.from_model(TenantUser))
+def test_user_delete(instance: TenantUser) -> None:
+    """Tests tenants.models.UserProfile.delete scenarios."""
+    # Test UserProfile.delete()
+    with pytest.raises(
+        DeleteError,
+        match=r'delete_user\(\) should be used',
+    ):
+        instance.delete()
 
-        # Force drop UserProfile
-        instance.delete(force_drop=True)
-        assert TenantUser.objects.filter(email=instance.email).exists() is False
+    # Force drop UserProfile
+    instance.delete(force_drop=True)
+    assert TenantUser.objects.filter(email=instance.email).exists() is False
