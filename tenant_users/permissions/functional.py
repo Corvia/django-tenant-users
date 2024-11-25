70ae879591ec10054d73b752d29d076d78c8b3ea
scuriolus <llalba@student.42.fr>
2024-11-25 12:56:24 +0100

    refactor: use TENANT_CACHE_NAME constant instead of plain text

--- tenant_users/permissions/functional.py
+++ tenant_users/permissions/functional.py
@@ -1,6 +1,8 @@
 from django.db import connection
 from django.utils.functional import cached_property
 
+from tenant_users.tenants.models import TENANT_CACHE_NAME
+
 
 class tenant_cached_property(cached_property):  # noqa: N801
     """A tenant-aware implementation of Django's cached_property decorator.
@@ -20,10 +22,10 @@ class tenant_cached_property(cached_property):  # noqa: N801
         if instance is None:
             return self
 
-        if "_tenant_cache" not in instance.__dict__:
-            instance.__dict__["_tenant_cache"] = {}
+        if TENANT_CACHE_NAME not in instance.__dict__:
+            instance.__dict__[TENANT_CACHE_NAME] = {}
 
-        tenant_cache = instance.__dict__["_tenant_cache"]
+        tenant_cache = instance.__dict__[TENANT_CACHE_NAME]
         current_schema = connection.schema_name
 
         if current_schema not in tenant_cache:
@@ -33,4 +35,3 @@ class tenant_cached_property(cached_property):  # noqa: N801
             tenant_cache[current_schema][self.name] = self.func(instance)
 
         return tenant_cache[current_schema][self.name]
-
--- tenant_users/tenants/models.py
+++ tenant_users/tenants/models.py
@@ -30,6 +30,8 @@ TENANT_DELETE_ERROR_MESSAGE = (
     "calling delete on tenant instance is not supported, call delete_tenant instead"
 )
 
+TENANT_CACHE_NAME = "_tenant_cache"
+
 
 class InactiveError(Exception):
     pass
