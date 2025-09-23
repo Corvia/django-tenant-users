d505e307f5f4d2971b66c69cecc297c5f9ca6517
Mike <mike@zivix.com>
2025-09-23 08:06:51 -0700

    test(tenants): add failing test for schema_required connection state management

--- django_test_app/companies/models.py
+++ django_test_app/companies/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django_tenants.models import DomainMixin
 
-from tenant_users.tenants.models import TenantBase
+from tenant_users.tenants.models import TenantBase, schema_required
 
 _NameFieldLength = 64
 
@@ -12,6 +12,11 @@ class Company(TenantBase):
     name = models.CharField(max_length=_NameFieldLength)
     description = models.TextField()
     type = models.CharField(max_length=100, default="type1")
+
+    @schema_required
+    def test_schema_method(self):
+        """Simple test method to validate @schema_required behavior."""
+        return f"Called on tenant: {self.schema_name}"
 
 
 class Domain(DomainMixin):
