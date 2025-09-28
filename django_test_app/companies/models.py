2a4f326e215729c2f733babaa322b1909b453f70
Mike <mike@zivix.com>
2025-09-28 13:17:33 -0700

    feat(test-app): add initial type annotations to companies and users models

--- django_test_app/companies/models.py
+++ django_test_app/companies/models.py
@@ -1,17 +1,34 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 from django.db import models
 from django_tenants.models import DomainMixin
 
+from django_test_app.users.models import TenantUser
 from tenant_users.tenants.models import TenantBase, schema_required
 
 _NameFieldLength = 64
 
+if TYPE_CHECKING:
+    from django.db.models.expressions import Combinable
 
-class Company(TenantBase):
+
+class Company(TenantBase[TenantUser]):
     """Test Tenant object."""
 
     name = models.CharField(max_length=_NameFieldLength)
     description = models.TextField()
     type = models.CharField(max_length=100, default="type1")
+
+    # ---- Typing Examples for django-tenant-users users ----
+    # If you want typing in your app, add annotations like these to your models.
+
+    # Type the default manager to return the correct model type
+    objects: models.Manager[Company] = models.Manager()
+
+    # Type the ForeignKey with union for expressions and the related model
+    owner: models.ForeignKey[TenantUser | Combinable, TenantUser]
 
     @schema_required
     def test_schema_method(self):
--- django_test_app/users/models.py
+++ django_test_app/users/models.py
@@ -1,12 +1,18 @@
+from __future__ import annotations
+
 import uuid
+from typing import TYPE_CHECKING, ClassVar
 
 from django.conf import settings
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-from tenant_users.tenants.models import UserProfile
+from tenant_users.tenants.models import UserProfile, UserProfileManager
 
 _NameFieldLength = 64
+
+if TYPE_CHECKING:
+    from django_test_app.companies.models import Company
 
 
 class TenantUser(UserProfile):
@@ -14,9 +20,22 @@ class TenantUser(UserProfile):
 
     name = models.CharField(max_length=_NameFieldLength, blank=True)
 
+    # ---- Typing Examples for django-tenant-users users ----
+    # If you want typing in your app, add annotations like these to your models.
+
+    # Type the custom manager with the specific model type
+    objects: ClassVar[UserProfileManager[TenantUser]]
+
+    # Type the reverse accessor created by Company.owner
+    company_set: models.Manager[Company]
+
+    # Be more specific since we type as [Any, Any] in UserProfile base class
+    tenants: models.ManyToManyField[Company, Company]
+
 
 class GuidUser(UserProfile):
     guid = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    name = models.CharField(max_length=_NameFieldLength, blank=True)
     tenants = models.ManyToManyField(
         settings.TENANT_MODEL,
         verbose_name=_("tenants"),
